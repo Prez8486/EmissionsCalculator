036c62214964e1a617a89f5a0f42476ebebc232b
nprez <nprez@DESKTOP-UB1KI4V>
2025-11-04 00:10:09 +1100

    Test

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@
         <router-link to="/leaderboard">Leaderboard</router-link>
         <router-link to="/Comparison">Comparison</router-link>
         <router-link to="/info">Information Page</router-link>
-        <router-link to="/timetable">Timetable</router-link>
+        <router-link to="/timetable">PTV Timetable</router-link>
         <router-link to="/friendspage">Friends Page</router-link>
         <button @click="showSettings = true" class="settings-btn">Settings</button>
         <router-link
